7a83def74752a68da633f32c1e45fdb6c83f752c
Naum Azeredo <naumazeredo@gmail.com>
2018-01-12 07:16:22 -0300

    Build basic bot layout

--- TODO.md
+++ TODO.md
@@ -4,6 +4,7 @@
 - [ ] Path finding
 - [ ] Floodfill (total kriptonite)
 - [ ] Improve tinyviewer
+- [ ] Build a state machine(?)
 
 ## Strategy
 
@@ -21,6 +22,9 @@
   - [ ] Have 8 workers to send to Mars
 - [ ] Rocket done
   - [ ] Load 8 workers and send to Mars
+  - [ ] Calculate better location to launch rocket (Get the round the rocket
+      will arrive, get asteroids karbonite + initial values to know the best
+      component and the best location on the component)
 
 
 ## Tests
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// TODO: Verify which includes are needed
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -5,37 +6,158 @@
 #include <assert.h>
 #include <random>
 #include <functional>
+#include <map>
 
 #include "bc.hpp"
 
+#define debug() printf("line %d", __LINE__); fflush(stdout);
+
+const int TOTAL_PLANETS    = 2;
+const int TOTAL_UNIT_TYPES = 7;
+
 using namespace bc;
+using namespace std;
+
+enum { WE, THEM };
+
+/*
+State machine to build better strategy
+
+GameState: the general strategy
+  responsible for:
+    - queueing researches
+    - managing rocket travels
+    - managing resources (?)
+
+  FactoryEra: building factories as soon as possible
+  RocketEra: building rocket to travel
+  EarthEra: surviving on Earth (and possibly having units on Mars)
+  MarsEra: surviving solely on Mars (required after round 750)
+
+
+AggressiveState: the army strategy
+  responsible for:
+    - controlling which army to build
+    - controlling size of the army
+    - controlling army strategies
+
+  Pacifist: survive and protect
+  GenghisKhan: destroy the enemy!
+
+*/
+
+class StateMachine {
+public:
+  StateMachine() :
+      gc {},
+      my_team { gc.get_team() },
+      earth { gc.get_earth_map() },
+      mars { gc.get_mars_map() }
+  {
+    game_state = GameState::FactoryEra;
+    aggr_state = AggressiveState::Pacifist;
+  }
+
+  void start() {
+  }
+
+  void step() {
+    get_round_data();
+    printf("round %d %u\n", round, karbonite);
+
+    gs_transitions[game_state]();
+    as_transitions[aggr_state]();
+
+    fflush(stdout);
+    gc.next_turn();
+  }
+
+private:
+  // State machine code
+  enum GameState {
+    FactoryEra,
+    RocketEra,
+    EarthEra,
+    MarsEra
+  };
+
+  enum AggressiveState {
+    Pacifist,
+    GenghisKhan
+  };
+
+  // IMPORTANT: Same order as enum!
+  const vector<function<void()>> gs_transitions = {
+    // FactoryEra
+    [this] {
+      if (units[WE][Factory].size() > 0)
+        game_state = GameState::RocketEra;
+    },
+    [this] {},
+    [this] {},
+    [this] {}
+  };
+
+  // IMPORTANT: Same order as enum!
+  const vector<function<void()>> as_transitions = {
+    [this] {},
+    [this] {}
+  };
+
+  // XXX: Use separate states?
+  GameState       game_state;
+  AggressiveState aggr_state;
+
+  // TODO: create state machine transitions
+
+  // --------------------
+
+  // Game code
+  GameController gc;
+
+  // Constant for whole game
+  const Team my_team;
+  const PlanetMap& earth, mars;
+
+  // Changed every round
+  unsigned round;
+  unsigned karbonite;
+
+  vector<vector<pair<bool, unsigned>>> planets[TOTAL_PLANETS];
+
+  vector<Unit> units[2][TOTAL_UNIT_TYPES];
+
+  void get_units() {
+    // Clear units
+    for (int i = 0; i < 2; i++)
+      for (int j = 0; j < TOTAL_UNIT_TYPES; j++)
+        units[i][j].clear();
+
+    auto all_units = gc.get_units();
+    for (auto unit : all_units)
+      units[!(unit.get_team() == my_team)][unit.get_unit_type()].push_back(unit);
+  }
+
+  void get_round_data() {
+    round = gc.get_round();
+    karbonite = gc.get_karbonite();
+
+    get_units();
+  }
+  // ---------
+};
 
 
 int main() {
-  printf("Player C++ bot starting\n");
-  printf("Connecting to manager...\n");
-  std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution (0,8);
-  auto dice = std::bind ( distribution , generator );
-  // Most methods return pointers; methods returning integers or enums are the only exception.
-  GameController gc;
-  gc.queue_research(Knight);
-  gc.queue_research(Knight);
-  gc.queue_research(Knight);
+  printf("d=] starting! Get ready to be pwned!\n");
 
-  if (bc_has_err()) {
-    // If there was an error creating gc, just die.
-    printf("Failed, dying.\n");
-    exit(1);
-  }
-  printf("Connected!\n");
+  StateMachine bot;
+  bot.start();
 
-  // loop through the whole game.
   while (true) {
-    unsigned round = gc.get_round();
-    printf("Round: %d\n", round);
+    bot.step();
 
-
+    /*
     auto units = gc.get_my_units();
     for (const auto unit : units) {
       const unsigned id = unit.get_id();
@@ -56,26 +178,27 @@ int main() {
       }
 
       if (unit.get_location().is_on_map()){
-      // Calls on the controller take unit IDs for ownership reasons.
+        // Calls on the controller take unit IDs for ownership reasons.
         const auto locus = unit.get_location().get_map_location();
         const auto nearby = gc.sense_nearby_units(locus, 2);
-        for ( auto place : nearby ){
+        for (auto place : nearby) {
           //Building 'em blueprints
           if(gc.can_build(id, place.get_id()) && unit.get_unit_type() == Worker){
             gc.build(id, place.get_id());
             continue;
           }
           //Attacking 'em enemies
-          if( place.get_team() != unit.get_team() and
+          if (place.get_team() != unit.get_team() and
               gc.is_attack_ready(id) and
-              gc.can_attack(id, place.get_id()) ){
-              gc.attack(id, place.get_id());
-              continue;
+              gc.can_attack(id, place.get_id())) {
+            gc.attack(id, place.get_id());
+            continue;
           }
         }
       }
 
       Direction d = (Direction) dice();
+
       // Placing 'em blueprints
       if(gc.can_blueprint(id, Factory, d) and gc.get_karbonite() > unit_type_get_blueprint_cost(Factory)){
         gc.blueprint(id, Factory, d);
@@ -83,12 +206,6 @@ int main() {
         gc.move_robot(id,d);
       }
     }
-    // this line helps the output logs make more sense by forcing output to be sent
-    // to the manager.
-    // it's not strictly necessary, but it helps.
-    // pause and wait for the next turn.
-    fflush(stdout);
-    gc.next_turn();
+    */
   }
-  // I'm convinced C++ is the better option :)
 }
